e200316ebb4fbb650efa0a8e4fb5b602e62ffdbd
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-08-30 18:44:28 -0300

    Ajustes de configurações do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
-      
     },
   },
 });
