45cef555740cc74f0cd658cc1ca95aa7ee6ec0ca
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-09-08 17:09:15 -0300

    Criação dos testes de produtos

---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,29 @@
+/// <reference types = "cypress"/>
+
+let site = 'http://lojaebac.ebaconline.art.br/produtos/';
+
+describe('Funcionalidade Página de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit(site);
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class = "product-block grid"]')
+            .first()
+            .click()
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        cy.get('[class = "product-block grid"]')
+            .first()
+            .click()
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.plus').click()
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+    });
+
+});
